fa07df44a9a670929e765eaf1b57c08a401b0cf4
WhiteSinner0 <white.antonsm3462@gmail.com>
2026-08-07 20:34:54 +0300

    fix: stop sputnik UI reset in multiplayer

--- src/main/java/dev/devce/rocketnautics/client/SputnikClientUI.java
+++ src/main/java/dev/devce/rocketnautics/client/SputnikClientUI.java
@@ -1,17 +1,26 @@
 package dev.devce.rocketnautics.client;
 
 import dev.devce.rocketnautics.content.blocks.SputnikBlockEntity;
-import dev.devce.websnodelib.client.ui.WNodeScreen;
 import dev.devce.rocketnautics.network.SputnikNodeSyncPayload;
+import dev.devce.websnodelib.api.WGraph;
+import dev.devce.websnodelib.client.ui.WNodeScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 public class SputnikClientUI {
     public static void openNodeScreen(SputnikBlockEntity blockEntity) {
+        WGraph editableGraph = new WGraph();
+        editableGraph.setContext(blockEntity);
+        if (blockEntity.getLevel() != null) {
+            editableGraph.setRegistries(blockEntity.getLevel().registryAccess());
+        }
+        editableGraph.load(blockEntity.graph.save());
+        editableGraph.setContext(blockEntity);
+
         Minecraft.getInstance().setScreen(new WNodeScreen(
-            Component.literal("Flight Computer"), 
-            blockEntity.graph,
+            Component.literal("Flight Computer"),
+            editableGraph,
             (tag) -> PacketDistributor.sendToServer(new SputnikNodeSyncPayload(blockEntity.getBlockPos(), tag)),
             null
         ));
