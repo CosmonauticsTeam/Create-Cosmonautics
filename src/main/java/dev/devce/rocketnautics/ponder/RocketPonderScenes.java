0c26174c6586b3d37951e87cc54d1edfd013f1c7
Alexander Rios <flussen0@gmail.com>
2026-08-21 14:12:32 +0200

    feat: add Ponder scenes and localization for rocket and creative thrusters

--- src/main/java/dev/devce/rocketnautics/ponder/RocketPonderScenes.java
+++ src/main/java/dev/devce/rocketnautics/ponder/RocketPonderScenes.java
@@ -14,63 +14,51 @@ import net.minecraft.world.phys.Vec3;
 public class RocketPonderScenes {
 
     public static void thrusterIntro(SceneBuilder scene, SceneBuildingUtil util) {
-        scene.title("rocket_thruster", "Rocket Thruster: Power and Fueling"); 
+        scene.title("rocket_thruster", "Rocket Thruster: Fuel and Thrust");
         scene.configureBasePlate(0, 0, 5);
-        scene.showBasePlate();
-        scene.idle(10);
 
         BlockPos thrusterPos = util.grid().at(2, 1, 2);
-        BlockPos tankPos = util.grid().at(2, 1, 1); 
+        BlockPos tankPos = util.grid().at(2, 1, 1);
 
-        
         scene.world().showSection(util.select().everywhere(), Direction.DOWN);
-        scene.idle(20);
+        scene.idle(30);
 
-        scene.overlay().showText(60)
+        scene.overlay().showText(100)
             .text("rocketnautics.ponder.rocket_thruster.text_1")
             .pointAt(util.vector().topOf(thrusterPos))
             .placeNearTarget();
-        scene.idle(70);
+        scene.addKeyframe();
+        scene.idle(120);
 
-        
-        scene.overlay().showText(70)
-            .text("rocketnautics.ponder.rocket_thruster.text_5") 
+        scene.overlay().showText(110)
+            .text("rocketnautics.ponder.rocket_thruster.text_2")
             .pointAt(util.vector().topOf(tankPos))
             .placeNearTarget();
-        scene.idle(80);
+        scene.idle(130);
 
-        scene.overlay().showText(70)
-            .text("rocketnautics.ponder.rocket_thruster.text_6") 
-            .pointAt(util.vector().centerOf(tankPos))
+        scene.overlay().showText(110)
+            .text("rocketnautics.ponder.rocket_thruster.text_3")
+            .pointAt(util.vector().topOf(tankPos))
             .placeNearTarget();
-        scene.idle(80);
+        scene.idle(130);
 
-        
-        scene.overlay().showControls(util.vector().topOf(thrusterPos), Pointing.DOWN, 40).rightClick();
-        scene.idle(10);
-        scene.overlay().showText(60)
-            .text("rocketnautics.ponder.rocket_thruster.text_2")
+        scene.overlay().showCenteredScrollInput(thrusterPos, Direction.UP, 60);
+        scene.overlay().showText(110)
+            .text("rocketnautics.ponder.rocket_thruster.text_4")
             .pointAt(util.vector().topOf(thrusterPos))
             .placeNearTarget();
-        scene.idle(70);
+        scene.addKeyframe();
+        scene.idle(130);
 
-        
-        
-        scene.effects().emitParticles(util.vector().centerOf(thrusterPos).add(0, -0.7, 0), scene.effects().particleEmitterWithinBlockSpace(net.minecraft.core.particles.ParticleTypes.FLAME, new Vec3(0, -1, 0)), 10, 5);
-        
-        scene.overlay().showText(60)
-            .text("rocketnautics.ponder.rocket_thruster.text_3")
-            .pointAt(util.vector().centerOf(thrusterPos))
-            .placeNearTarget();
-        scene.idle(80);
-
-        
-        scene.overlay().showText(80)
-            .colored(net.createmod.ponder.api.PonderPalette.RED)
-            .text("rocketnautics.ponder.rocket_thruster.text_4")
+        scene.effects().indicateSuccess(thrusterPos);
+        scene.effects().emitParticles(util.vector().centerOf(thrusterPos).add(0, -0.7, 0),
+            scene.effects().particleEmitterWithinBlockSpace(net.minecraft.core.particles.ParticleTypes.FLAME, new Vec3(0, -0.2, 0)), 0.7f, 100);
+        scene.overlay().showText(110)
+            .text("rocketnautics.ponder.rocket_thruster.text_5")
             .pointAt(util.vector().blockSurface(thrusterPos, Direction.DOWN))
             .placeNearTarget();
-        scene.idle(90);
+        scene.addKeyframe();
+        scene.idle(130);
     }
 
     public static void creativeThrusterThrottle(SceneBuilder scene, SceneBuildingUtil util) {
